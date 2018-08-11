59f6d8d6437c9e094b6f94e8bc8dfa59dcb61210
neha8092 <42293630+neha8092@users.noreply.github.com>
2018-08-11 15:29:18 +0530

    Update git_example.py

--- git_example.py
+++ git_example.py
@@ -1,18 +1,18 @@
-```
-This python file si a goood starting pouint.
-Activation Funstions
-```
+
+#This python file is a good starting point.
+#Activation Functions
+
 
 #libraries
 import numpy as np
 
 def sigmoid(x):
   # enter code below
-  return (1 / (1 + numpy.exp(-x)))
+  return (1 / (1 + np.exp(-x)))
 
 def tanh(x):
   # enter code below
   
 def relu(x):
-  # entr code below
+  # enter code below
   
